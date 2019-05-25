d62a7bd017a16a72e4df5129db48deee3e204081
Danil Starovoytov <danil.starovoytov@gmail.com>
2019-05-26 00:49:31 +0300

    add mongo

--- src/main/java/com/brolabs/admanager/server/ServerApplication.java
+++ src/main/java/com/brolabs/admanager/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.brolabs.admanager.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.brolabs.admanager.server.repo")
 public class ServerApplication {
 
 	public static void main(String[] args) {
