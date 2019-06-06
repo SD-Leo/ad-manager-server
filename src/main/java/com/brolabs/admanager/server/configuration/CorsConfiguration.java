96179374659961f2a23ed44bab74c9ac2681b00f
Danil Starovoytov <danil.starovoytov@gmail.com>
2019-06-06 23:46:12 +0300

    turn CORS off

---
+++ src/main/java/com/brolabs/admanager/server/configuration/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.brolabs.admanager.server.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 2019-06-05
+ *
+ * @author Starovoytov Danil
+ */
+@EnableWebMvc
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+}
